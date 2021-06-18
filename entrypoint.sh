cb30534e96d16a01515544d285817f5353db8758
YB <ybkim@2ndsyndrome.com>
2021-06-18 17:48:22 +0900

    Update script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 set -eu
 
-if test -z "$SLACK_BOT_TOKEN"; then
-  echo "Set the SLACK_BOT_TOKEN secret."
-  exit 1
-fi
+#if test -z "$SLACK_BOT_TOKEN"; then
+#  echo "Set the SLACK_BOT_TOKEN secret."
+#  exit 1
+#fi
 
 for argument in "$@"
 do 
